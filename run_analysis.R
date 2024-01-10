d1f687c99bc01784c10f9c13321f9f7dfcef4caa
thomas-storm <dr.storm@web.de>
2024-01-10 18:58:22 +0100

    Download implemented

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,17 @@
-# R script for Peer-graded Assignment: Getting and Cleaning Data Course Projec
+# R script for Peer-graded Assignment: Getting and Cleaning Data Course Project
+
+# setting up the main directors and "data" sub directory
+main_dir <- "C:/Users/stormth1/DataScience/HumanActivitySmartphones"
+setwd(main_dir)
+sub_dir <- "./data"
+
+# check if "data" sub directory exists, if not download and unzip file
+if (!file.exists(sub_dir)){
+        url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+        temp <- tempfile()
+        download.file(url, temp)
+        unzip(zipfile = temp, exdir = file.path(main_dir))
+        unlink(c(temp))
+        rm(temp)
+        file.rename("UCI HAR Dataset", "data")
+} else {print("data folder available")}
