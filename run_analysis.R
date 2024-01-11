2b8e1ba6913d24cda468c667e7d5bb6122f1fff8
thomas-storm <dr.storm@web.de>
2024-01-11 07:21:12 +0100

    read data added (tidy)

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,13 @@
 # R script for Peer-graded Assignment: Getting and Cleaning Data Course Project
 
-# setting up the main directors and "data" sub directory
-main_dir <- "C:/Users/stormth1/DataScience/HumanActivitySmartphones"
-setwd(main_dir)
+library(tidyverse)
+
+# set main directory and "data" sub directory
+# requires working directory is set correctly
+main_dir <- getwd()
 sub_dir <- "./data"
 
-# check if "data" sub directory exists, if not download and unzip file
+# check if "data" sub-directory exists, if not download and unzip file
 if (!file.exists(sub_dir)){
         url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
         temp <- tempfile()
@@ -15,3 +17,21 @@ if (!file.exists(sub_dir)){
         rm(temp)
         file.rename("UCI HAR Dataset", "data")
 } else {print("data folder available")}
+
+features <- read_table("./data/features.txt", 
+                     col_names = c("index","features"), 
+                     col_types = cols(
+                             index = col_double(),
+                             features = col_character()
+                                )
+                        )
+
+X_test <- read_table("./data/test/X_test.txt", 
+                     col_names = features$features
+                        )
+
+y_test <- read_table("./data/test/y_test.txt", 
+                     col_names = "activity", 
+                     col_types = cols(activity = col_double())
+                        )
+
