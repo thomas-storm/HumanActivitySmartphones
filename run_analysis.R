c67820f4d16d313266956ab50cfa6504552567ec
thomas-storm <dr.storm@web.de>
2024-01-11 23:30:17 +0100

    select mean/std variables

--- run_analysis.R
+++ run_analysis.R
@@ -22,15 +22,40 @@ features <- read_table("./data/features.txt",
                      col_names = c("index","features"), 
                      col_types = cols(
                              index = col_double(),
-                             features = col_character()
-                                )
+                             features = col_character())
+                        )
+
+activity_labels <- read_table("./data/activity_labels.txt", 
+                                col_names = c("index", "activity"), 
+                                col_types = cols(activity = col_character())
+                        )
+
+subject_test <- read_table("./data/test/subject_test.txt",
+                                col_names = "subject",
+                                col_types = cols(subject = col_double())
                         )
 
 X_test <- read_table("./data/test/X_test.txt", 
-                     col_names = features$features
+                     col_names = features$features,
+                     col_types = cols(.default = col_double())
                         )
 
 y_test <- read_table("./data/test/y_test.txt", 
+                     col_names = "activity", 
+                     col_types = cols(activity = col_double())
+                        )
+
+subject_train <- read_table("./data/train/subject_train.txt",
+                           col_names = "subject",
+                           col_types = cols(subject = col_double())
+                        )
+
+X_train <- read_table("./data/train/X_train.txt", 
+                     col_names = features$features,
+                     col_types = cols(.default = col_double())
+                        )
+
+y_train <- read_table("./data/train/y_train.txt", 
                      col_names = "activity", 
                      col_types = cols(activity = col_double())
                         )
